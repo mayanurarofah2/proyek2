eaa6dfaa4d4f4fa212e914fd075ba11cb03b0438
mayanurarofah2 <mayanurarofah2gmail.com>
2026-04-22 21:35:22 +0700

    terbru2

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
+    protected $model = User::class; // 🔥 WAJIB INI
     /**
      * The current password being used by the factory.
      */
